7401572e6de82a5909a4adadb44fc993384d2521
andrew6072 <leducanh017@gmail.com>
2024-04-10 00:31:22 +0300

    fix hyperparams, add demo gzt for vimq

--- GAIN/run_predict.sh
+++ GAIN/run_predict.sh
@@ -1,19 +1,10 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
-
-REPO=$PWD
-
-LAN=${1:-"en"}
-ENCODER_MODEL=${2:-"xlm-roberta-large"}
-DATA_DIR=${3:-"$REPO/data/EN-English"}
-OUT_DIR=${4:-"$REPO/output/"}
-
-model_name="ner_${LAN}"
-base_dir=${REPO}/../
-test_file=${DATA_DIR}/${LAN}_test.conll
-gazetteer_path=${base_dir}/gazetteer_demo/${LAN}
-
-model_file_path=${REPO}/output/ner_en/lightning_logs/version_5/checkpoints/ner_en_timestamp_1710514359.5695827_final.ckpt
-
-python -m do_predict --test "$test_file" --gazetteer "$gazetteer_path" --out_dir "$OUT_DIR" --model_name "$model_name" --gpus 0 \
-                                    --encoder_model "$ENCODER_MODEL" --model "$model_file_path" --batch_size 32
+python -m do_predict \
+    --test ./data/EN-English/en_test.conll \
+    --gazetteer ../gazetteer_demo/en 
+    --out_dir ./output/ \
+    --model_name ner_multiconer_en \
+    --gpus 1 \
+    --encoder_model xlm-roberta-large \
+    --model ./output/ner_en/lightning_logs/version_5/checkpoints/ner_en_timestamp_1710514359.5695827_final.ckpt \
+    --batch_size 32 \
+    --iob_tagging wnut
--- GAIN/run_train.sh
+++ GAIN/run_train.sh
@@ -1,19 +1,12 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
-
-REPO=$PWD
-
-LAN=${1:-"en"}
-ENCODER_MODEL=${2:-"xlm-roberta-large"}
-DATA_DIR=${3:-"$REPO/data/EN-English"}
-OUT_DIR=${4:-"$REPO/output/"}
-
-base_dir=${REPO}/../
-train_file=${DATA_DIR}/${LAN}_train.conll
-dev_file=${DATA_DIR}/${LAN}_dev.conll
-gazetteer_path=${base_dir}/gazetteer_demo/${LAN}
-model_name="ner_${LAN}"
-
-python -m train_model --train "$train_file" --dev "$dev_file" --gazetteer "$gazetteer_path" --out_dir "$OUT_DIR" \
-                      --model_name "$model_name" --gpus 1 --epochs 1 --encoder_model "$ENCODER_MODEL" --batch_size 32 \
-                      --lr 0.02
+python -m train_model \
+    --train ./data/EN-English/en_train.conll \
+    --dev ./data/EN-English/en_dev.conll \
+    --gazetteer ../gazetteer_demo/en \
+    --out_dir ./output/ \
+    --model_name ner_multiconer_en \
+    --gpus 1 \
+    --epochs 20 \
+    --encoder_model xlm-roberta-large \
+    --batch_size 32 \
+    --lr 0.00002 \
+    --iob_tagging wnut
