5abdd19513469644be097100c5f930214371fcc6
andrew6072 <leducanh017@gmail.com>
2024-04-14 17:34:53 +0300

    fix bug in GAIN/run_predict.sh

--- GAIN/run_predict.sh
+++ GAIN/run_predict.sh
@@ -1,10 +1,10 @@
 python -m do_predict \
     --test ./data/EN-English/en_test.conll \
-    --gazetteer ../gazetteer_demo/en 
+    --gazetteer ../gazetteer_demo/en \
     --out_dir ./output/ \
     --model_name ner_multiconer_en \
-    --gpus 1 \
+    --gpus 0 \
     --encoder_model xlm-roberta-large \
-    --model ./output/ner_en/lightning_logs/version_5/checkpoints/ner_en_timestamp_1710514359.5695827_final.ckpt \
+    --model ./output/ner_en/lightning_logs/version_4/checkpoints/ner_en_timestamp_1708356296.0393934_final.ckpt \
     --batch_size 32 \
     --iob_tagging wnut
